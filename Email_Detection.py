bbba4b8e27c22e7da4d7a9a5d53f517f06fdd75b
Amogha-Upadyaya <120311753+Amogha-Upadyaya@users.noreply.github.com>
2023-06-20 14:24:01 +0000

    Email detection  - Copy.ipynb

---
+++ Email_Detection.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.show()
+
+import seaborn as sns
+import nltk
+from nltk.corpus import stopwords
+
+import string
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+import re
