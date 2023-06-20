a4bd0d685c4595e03d87f5655dc64db04110a1d8
Amogha-Upadyaya <120311753+Amogha-Upadyaya@users.noreply.github.com>
2023-06-20 14:24:18 +0000

    Email_Detection.py

--- Email_Detection.py
+++ Email_Detection.py
@@ -12,5 +12,22 @@ import string
 import warnings
 warnings.filterwarnings('ignore')
 
-
 import re
+
+df = pd.read_csv("./spam.csv")
+
+print(df.head)
+
+print(df.Label.value_counts())
+
+print("There are {} rows and {} columns are present in the Data Set".format(df.shape[0],df.shape[1]))
+
+print(df.info())
+
+print(df.types)
+
+print(df.describe())
+
+print(df.groupby('Label').describe().T)
+
+print(df.isnull().sum())
